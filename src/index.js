0dcc46f9351490cdabc6abfea9cfbc1ff006cb81
Hannes Rahm <me@hannesrahm.com>
2019-07-30 10:25:23 +0200

    Use async-await for a cleaner index.js

--- src/index.js
+++ src/index.js
@@ -3,39 +3,35 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import parser from 'csv-parse'
+import csv from 'async-csv'
 import dexie from 'dexie'
 
 const DB_VERSION = 1
+async function bootstrap() {
+	// Parse codebook into javascript object
+	var response = await fetch('/codebook.csv')
+	var codebook = await response.text()
+	var csvString = await csv.parse(codebook)
+	let columns = csvString[0]
+	let items = []
+	for (let i = 1; i < csvString.length; i++) {
+		let o = {}
+		for (let j = 0; j < columns.length; j++)
+			o[columns[j]] = csvString[i][j]
+		items.push(o)
+	}
+	
+	// Initialize Dexie database
+	let db = new dexie('TAFT')
+	let store = {
+		records: '++uid,' + items.map(d => d.name).join(',')
+	}
+	
+	db.version(DB_VERSION).stores(store)
+	
+	// Bootstrap the 'App'
+	ReactDOM.render(<App codebook={items} db={db} />, document.getElementById('root'))
+}
 
-// Parse codebook into javascript object
-fetch('/codebook.csv')
-	.then(d => {
-		d.text()
-			.then(e => {
-				parser(e, {}, (err, output) => {
-					let columns = output[0]
-					let items = []
-					for (let i = 1; i < output.length; i++) {
-						let o = {}
-						for (let j = 0; j < columns.length; j++)
-							o[columns[j]] = output[i][j]
-						items.push(o)
-					}
-
-					// Initialize Dexie database
-					let db = new dexie('TAFT')
-					let store = {
-						records: '++uid,' + items.map(d => d.name).join(',')
-					}
-				
-					db.version(DB_VERSION).stores(store)
-					
-					// Bootstrap the 'App'
-					ReactDOM.render(<App codebook={items} db={db}/>, document.getElementById('root'))
-				})
-			})
-	})
-
-
-serviceWorker.register();
+bootstrap()
+serviceWorker.register()
