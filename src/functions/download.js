469f0851a9aca422e7528bedd8a959d4846a5034
Hannes Rahm <me@hannesrahm.com>
2019-09-19 14:18:28 +0200

    Firefox requires the element to be added to the document for a download to take place

--- src/functions/download.js
+++ src/functions/download.js
@@ -8,9 +8,13 @@ export default function(data, filename, prefix = 'data:application/octet-stream,
 
 	// Create element with href and download attributes
 	let el = document.createElement('A')
+	document.body.appendChild(el)
 	el.setAttribute('href', prefix + encoded)
 	el.setAttribute('download', filename)
 
 	// Virtually click it to have the browser start download
 	el.click()
+
+	// Remove element
+	el.remove()
 }
