c03e1f52912e5357f320ac4d6bc84d21c5b95199
Hannes Rahm <me@hannesrahm.com>
2019-11-18 14:43:26 +0100

    Unify disabled-property in FieldEditor

--- src/components/FieldEditor.js
+++ src/components/FieldEditor.js
@@ -160,10 +160,9 @@ class FieldEditor extends React.Component {
 				<input
 					className="input is-small"
 					type="number"
-					disabled={d.input !== 'yes'}
+					disabled={d.input !== 'yes' || this.props.disabled}
 					placeholder={unlabeled ? d.label : ''}
 					value={this.state.value}
-					disabled={this.props.disabled}
 					onChange={e => this.changeValueText(e)}
 					onFocus={e => this.onFocus()}
 					onBlur={e => this.onBlur()}
