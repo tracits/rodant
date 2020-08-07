d63b5fbbb6e09463a9278ff42cb8456edc80cc7c
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-08-08 00:35:57 +0200

    Build initial docs

---
+++ docs/_build/html/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'Successfully installed Jinja2-2.11.2 Pygments-2.6.1 alabaster-0.7.12 babel-2.8.0 docutils-0.16 imagesize-1.2.0 packaging-20.4 pyparsing-2.4.7 pytz-2020.1 snowballstemmer-2.0.0 sphinx-3.1.2 sphinxcontrib-applehelp-1.0.2 sphinxcontrib-devhelp-1.0.2 sphinxcontrib-htmlhelp-1.0.3 sphinxcontrib-jsmath-1.0.1 sphinxcontrib-qthelp-1.0.3 sphinxcontrib-serializinghtml-1.1.4',
+    LANGUAGE: 'martin@mkII:~/projects/rodant$ martin@mkII:~/projects/rodant$',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
