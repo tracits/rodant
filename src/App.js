218eba7b33ccb64d10f71f14bffd998154da85ed
karnell schultz <karnell.schultz@izettle.com>
2020-09-25 19:25:30 +0200

    refactor app class to func

--- src/App.js
+++ src/App.js
@@ -13,47 +13,40 @@ import Nav from './components/Nav'
  * Container for the application.
  * Uses react-router to select what page to show.
  */
-class App extends React.Component {
-	render() {
-		// Create component factories for the routes below
-		let recordPicker = () => (
-			<RecordPicker
-				db={this.props.db}
-				codebook={this.props.codebook}
-				config={this.props.config}
-			/>
-		)
-		let recordEditor = (match) => (
-			<RecordEditor
-				db={this.props.db}
-				codebook={this.props.codebook}
-				uid={match.match.params.uid}
-				config={this.props.config}
-			/>
-		)
-		let doubleEntry = (match) => (
-			<RecordEditor
-				db={this.props.db}
-				codebook={this.props.codebook}
-				uid={match.match.params.uid}
-				double-entry="true"
-				config={this.props.config}
-			/>
-		)
+function App({ db, codebook, config }) {
+	// Create component factories for the routes below
+	let recordPicker = () => (
+		<RecordPicker db={db} codebook={codebook} config={config} />
+	)
+	let recordEditor = (match) => (
+		<RecordEditor
+			db={db}
+			codebook={codebook}
+			uid={match.match.params.uid}
+			config={config}
+		/>
+	)
+	let doubleEntry = (match) => (
+		<RecordEditor
+			db={db}
+			codebook={codebook}
+			uid={match.match.params.uid}
+			double-entry="true"
+			config={config}
+		/>
+	)
 
-		// Use React Router to select which page to show from the url
-		return (
-			<Router basename={process.env.PUBLIC_URL}>
-				<Nav name={this.props.config.name} version={packagejson.version} />
-
-				<div className="container">
-					<Route path="/" exact component={recordPicker} />
-					<Route path="/record/:uid" component={recordEditor} />
-					<Route path="/complete/:uid" component={doubleEntry} />
-				</div>
-			</Router>
-		)
-	}
+	// Use React Router to select which page to show from the url
+	return (
+		<Router basename={process.env.PUBLIC_URL}>
+			<Nav name={config.name} version={packagejson.version} />
+			<div className="container">
+				<Route path="/" exact component={recordPicker} />
+				<Route path="/record/:uid" component={recordEditor} />
+				<Route path="/complete/:uid" component={doubleEntry} />
+			</div>
+		</Router>
+	)
 }
 
 export default App
